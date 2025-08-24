148fe86b8252666933057649d91b49cd4662bfdf
haru1489248 <kyudo.9248@icloud.com>
2025-08-25 01:28:04 +0900

    complete section_30

---
+++ ch4/section_30.py
@@ -0,0 +1,18 @@
+import MeCab
+
+text = """
+メロスは激怒した。
+必ず、かの邪智暴虐の王を除かなければならぬと決意した。
+メロスには政治がわからぬ。
+メロスは、村の牧人である。
+笛を吹き、羊と遊んで暮して来た。
+けれども邪悪に対しては、人一倍に敏感であった。
+"""
+
+tagger = MeCab.Tagger()
+node = tagger.parseToNode(text)
+while node:
+    feature = node.feature.split(',')[0]
+    if feature == "動詞":
+        print(node.surface)
+    node = node.next
